0f06843f357417e0d5502f49266e77f6e8fae8d1
St@rLeG <oleg.starynski@gmail.com>
2023-01-20 19:55:39 +0300

    update

--- source/main.cpp
+++ source/main.cpp
@@ -8,13 +8,8 @@ int main()
 	ConverterJSON cj;
 
 	std::cout << cj.configJson.config.name << std::endl;
-	std::cout << cj.configJson.config.version << std::endl;
-	std::cout << cj.configJson.config.max_responses << std::endl;
+	std::cout << "Program version: " << cj.configJson.config.version << std::endl;
 
-	for(int i = 0; i < cj.configJson.files.size(); i++)
-	{
-		std::cout << cj.configJson.files[i] << std::endl;
-	}
 
 
 
