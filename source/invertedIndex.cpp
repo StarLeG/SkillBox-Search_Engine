ca1d7e66c04b98f1cfbffd7adb03aebffabdab93
StarLeG <oleg.starynski@gmail.com>
2023-01-23 15:34:09 +0300

    Update metod UpdateDocumentBase()

--- source/invertedIndex.cpp
+++ source/invertedIndex.cpp
@@ -3,38 +3,59 @@
 void InvertedIndex::UpdateDocumentBase(std::vector<std::string> input_docs)
 {
 	std::string buffer;
-	if(input_docs.empty())
+	size_t doc_id = 0;
+	std::vector<Entry> entry;
+
+	if (input_docs.empty())
 	{
 		std::cerr << "Warring!!! Indexing: no content in docs content base." << std::endl;
 		return;
 	}
-		for(auto &it: input_docs)
+	for (auto& it: input_docs)
+	{
+		for (int i = 0; i < it.length(); i++)
 		{
-			for(int i = 0; i < it.length(); i++)
-			{
-				buffer += std::tolower(it[i]);
-			}
-			this->docs.emplace_back(buffer);
-			buffer.clear();
+			buffer += std::tolower(it[i]);
 		}
+		this->docs.emplace_back(buffer);
+		buffer.clear();
+	}
 
-		for(auto &it : this->docs)
+	freq_dictionary.clear();
+
+	for (auto& it: this->docs)
+	{
+		std::string buffer = it;
+		std::stringstream ss{ buffer };
+		buffer.clear();
+		while (ss >> buffer)
 		{
-			std::string buffer = it;
-			std::stringstream ss{ buffer};
-			buffer.clear();
-			while(ss >> buffer)
+			auto iterator = freq_dictionary.find(buffer);
+			if (iterator == freq_dictionary.end())
 			{
-				auto iterator = freq_dictionary.find(buffer);
+				entry.resize(1);
+				entry[0].doc_id = doc_id;
+				entry[0].count = 1;
+				freq_dictionary.emplace(buffer,entry);
+			}
+			else
+			{
 
 			}
 
 		}
+		doc_id++;
+
+	}
 
 
 }
 
-std::vector<Entry> InvertedIndex::GetWordCount(const std::string &word)
+std::vector<Entry> InvertedIndex::GetWordCount(const std::string& word)
 {
+	auto iterator = freq_dictionary.find(word);
+	if (iterator != freq_dictionary.end())
+	{
 
+	}
 }
