e4795931df4755e477f79f34577c3b844fee2579
St@rLeG <oleg.starynski@gmail.com>
2023-03-11 22:39:48 +0300

    update SearchServer

--- include/SearchServer.h
+++ include/SearchServer.h
@@ -1,24 +1,13 @@
 #pragma once
-
 #include "invertedIndex.h"
 #include <unordered_set>
-#include <algorithm>
-#include <utility>
-#include <vector>
-#include <string>
-#include <cmath>
-#include <map>
-#include <set>
 
-struct RelativeIndex
-{
-	size_t doc_id;
-	float rank;
-
-	bool operator==(const RelativeIndex& other) const
-	{
-		return (doc_id == other.doc_id && rank == other.rank);
-	}
+struct RelativeIndex{
+		size_t doc_id;
+		float rank;
+		bool operator ==(const RelativeIndex& other) const {
+			return (doc_id == other.doc_id && rank == other.rank);
+		}
 
 	RelativeIndex(size_t doc_id, float rank) : doc_id(doc_id), rank(rank)
 	{
@@ -26,8 +15,7 @@ struct RelativeIndex
 };
 
 
-class SearchServer
-{
+class SearchServer {
 public:
 /**
 * @param idx в конструктор класса передаётся ссылка на класс
@@ -35,8 +23,7 @@ InvertedIndex,
 * чтобы SearchServer мог узнать частоту слов встречаемых в
 запросе
 */
-	SearchServer(InvertedIndex& idx) : _index(idx){};
-
+	SearchServer(InvertedIndex& idx) : _index(idx){ };
 /**
 * Метод обработки поисковых запросов
 * @param queries_input поисковые запросы взятые из файла
@@ -46,7 +33,6 @@ requests.json
 */
 	std::vector<std::vector<RelativeIndex>> search(const
 	std::vector<std::string>& queries_input);
-
 private:
 	InvertedIndex _index;
 };
